510f4db71235091a413a44e17a1e486f5e0d1e14
Bcol3 <bcoletti08@gmail.com>
2021-05-31 23:39:05 -0300

    commit html ok

--- vagaProgramador/Formulario/index.aspx
+++ vagaProgramador/Formulario/index.aspx
@@ -4,15 +4,56 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
 </head>
 <body>
-    <form id="form1" runat="server"> <!-- Kayke viado -->
-        <div>
-            <h1>Bruno viadoooo</h1>
-            <h2> é mentira esse bilhete</h2>
-        </div>
+    <form id="form1" runat="server">
+        <!-- Kayke viado -->
+        <fieldset border="1" name="form1" id="form1">
+            <!--Formulário 1-->
+            <legend>Dados Pessoais</legend>
+            <asp:Label ID="nome" runat="server" Text="Label">Nome: <input id="nome_input" type="text" size="60"/>*</asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="endereco" runat="server" Text="Label">Endereço: <input id="endereco_input" type="text" size="90"/>*</asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="cidade" runat="server" Text="Label">Cidade: <input id="cidade_input" type="text" size="20"/>*</asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="estado" runat="server" Text="Label">Estado: 
+                <select id="estado_select" >
+                    <option id="estado_opcao">
+                        <!-- lista back end-->
+                    </option>
+                </select>
+           *</asp:Label>
+        </fieldset>
+        <br />
+        <br />
+        <br />
+        <fieldset border="1" name="form2" id="form2">
+            <!--Formulário 2-->
+            <legend>Dados Profissionais</legend>
+            <asp:Label ID="ocupacao" runat="server" Text="Label">Ocupação atual: <input id="Text1" type="text" size="20"/></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="cargo" runat="server" Text="cargo: "></asp:Label>
+            <asp:Label ID="analista" runat="server" Text="Label"><input id="analista_checkbox" type="checkbox" checked /> Analista de sistemas   |   </asp:Label>
+            <asp:Label ID="dba" runat="server" Text="Label"><input id="dba_checkbox" type="checkbox" /> DBA   |   </asp:Label>
+            <asp:Label ID="aspnet" runat="server" Text="Label"><input id="aspnet_checkbox" type="checkbox" /> Programador ASP.NET  |   </asp:Label>
+            <asp:Label ID="csharp" runat="server" Text="Label"><input id="csharp_checkbox" type="checkbox" /> Programador C#   |   </asp:Label>
+            <asp:Label ID="php" runat="server" Text="Label"><input id="php_checkbox" type="checkbox" /> Programador PHP</asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="cv" runat="server" Text="Label">Mini currículo: <textarea id="cv_textarea" rows="10" cols="30"></textarea></asp:Label>
+        </fieldset>
+        <br />
+        <br />
+        <input id="btnEnviar" type="submit" value="Enviar" />
+        <input id="btnExibir" type="button" value="Exibir" />
+        <input id="btnApagar" type="reset" value="Apagar" />
     </form>
 </body>
 </html>
--- vagaProgramador/Formulario/index.aspx.designer.cs
+++ vagaProgramador/Formulario/index.aspx.designer.cs
@@ -22,5 +22,113 @@ namespace vagaProgramador.Formulario
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle nome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label nome;
+
+        /// <summary>
+        /// Controle endereco.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label endereco;
+
+        /// <summary>
+        /// Controle cidade.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label cidade;
+
+        /// <summary>
+        /// Controle estado.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label estado;
+
+        /// <summary>
+        /// Controle ocupacao.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ocupacao;
+
+        /// <summary>
+        /// Controle cargo.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label cargo;
+
+        /// <summary>
+        /// Controle analista.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label analista;
+
+        /// <summary>
+        /// Controle dba.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label dba;
+
+        /// <summary>
+        /// Controle aspnet.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label aspnet;
+
+        /// <summary>
+        /// Controle csharp.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label csharp;
+
+        /// <summary>
+        /// Controle php.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label php;
+
+        /// <summary>
+        /// Controle cv.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label cv;
     }
 }
